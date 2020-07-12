95791383fd3244d469599d23f19c45ad23ff3937
Fanni Franssila <fanni.franssila@gmail.com>
2020-07-12 14:40:29 +0300

    Tehtiin loput osan 2 tehtävät

--- Osa2/puhelinluettelo/src/App.js
+++ Osa2/puhelinluettelo/src/App.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react'
-import axios from 'axios'
+import numberService from './services/numbers'
 
 const App = () => {
   const [persons, setPersons] = useState([
@@ -7,11 +7,13 @@ const App = () => {
   const [ newName, setNewName ] = useState('')
   const [ newNumber, setNewNumber ] = useState('')
   const [ newSearch, setNewSearch ] = useState('')
+  const [message, setMessage] = useState({text:null, type:'notify'})
 
   useEffect(() => {
-    axios
-      .get('http://localhost:3001/persons').then(response => {
-        setPersons(response.data)
+    numberService
+      .getAll()
+      .then(startPersons => {
+        setPersons(startPersons)
       })
   }, [])
 
@@ -35,23 +37,76 @@ const App = () => {
       const names = persons.map(person => person.name)
 
       if (names.includes(newName)) {
-          window.alert(`${newName} is already added to phonebook`)
-          return
+          const replace = window.confirm(`${newName} is already added to phonebook. Replace the old number with a new one?`)
+          
+          if (!replace) {
+            return
+          }
+
+          const id = persons.find(p => p.name === newName).id
+          numberService.update(id, num)
+            .then(returnedNumber => {
+              setPersons(persons.map(number => number.id !== id ? number : returnedNumber))
+              setNewName('')
+              setNewNumber('')
+              setMessage({text:`Updated ${num.name}`, type:'notify'})
+              setTimeout(() => {
+              setMessage({text:null, type:'notify'})
+              }, 5000)  
+            })
+            .catch(error => {
+              setPersons(persons.filter(p => p.id !== id))
+              setMessage({text:`Information of ${newName} has already been deleted from the server`, type:'error'})
+              setTimeout(() => {
+              setMessage({text:null, type:'notify'})
+              }, 5000)  
+            })
+            return
       }
 
-      setPersons(persons.concat(num))
-      setNewName('')
-      setNewNumber('')
+      numberService
+        .create(num)
+        .then(returnedNumber => {
+          setPersons(persons.concat(returnedNumber))
+          setNewName('')
+          setNewNumber('')
+          setMessage({text:`Added ${num.name}`, type:'notify'})
+          setTimeout(() => {
+            setMessage({text:null, type:'notify'})
+          }, 5000)  
+        }) 
+  }
+
+  const deleteNumber = (toDelete) => {
+    if (window.confirm(`Delete ${toDelete.name}?`)) {
+      numberService
+        .remove(toDelete.id)
+        .then(() => {
+          setPersons(persons.filter(person => person.id !== toDelete.id))
+          setMessage({text:`Deleted ${toDelete.name}`, type:'notify'})
+          setTimeout(() => {
+            setMessage({text:null, type:'notify'})
+          }, 5000)  
+        })
+        .catch(error => {
+          setPersons(persons.filter(person => person.id !== toDelete.id))
+          setMessage({text:`This number has already been deleted from the server`, type:'error'})
+          setTimeout(() => {
+            setMessage({text:null, type:'notify'})
+          }, 5000)  
+        })
+    }
   }
 
   return (
     <div>
       <h2>Phonebook</h2>
+      <Notification message = {message} />
       <Filter newSearch = {newSearch} handleSearchChange = {handleSearchChange} />
       <h2>Add a new number</h2>
       <PersonForm addNumber = {addNumber} newName = {newName} newNumber = {newNumber} handleNameChange = {handleNameChange} handleNumberChange = {handleNumberChange} />
       <h2>Numbers</h2>
-      <Persons persons = {personsToShow} />
+      <Persons persons = {personsToShow} remove = {deleteNumber} />
     </div>
   )
 
@@ -81,16 +136,28 @@ const Filter = (props) => {
 
 const NumberLine = (props) => {
     return (
-      <li>{props.person.name} {props.person.number}</li>
+      <li>
+        {props.person.name} {props.person.number} 
+        <button onClick = {props.remove}>Delete</button>
+      </li>
     )
 }
 
 const Persons = (props) => {
     return (
       <ul>
-          {props.persons.map(person => <NumberLine key={person.name} person = {person} />)}
+          {props.persons.map(person => <NumberLine key={person.name} person = {person} remove = {() => props.remove(person)} />)}
       </ul>
     )
+}
+
+const Notification = (props) => {
+  if (props.message.text === null) {
+    return null
+  }
+  return (
+    <div className = {props.message.type}>{props.message.text}</div>
+  )
 }
 
 export default App
